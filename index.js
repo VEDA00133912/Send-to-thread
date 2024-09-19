808a714f9f127602a8d82ddda0ac239ea97f4e11
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-19 14:10:06 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const { config } = require('dotenv');
+config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.login(process.env.token);
